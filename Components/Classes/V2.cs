d66a0c972c9615d121c375f5ac94a0b345755857
Justas <zhongli3137@gmail.com>
2025-06-06 18:27:51 +0300

    Add project files.

---
+++ Components/Classes/V2.cs
@@ -0,0 +1,20 @@
+﻿namespace Graplo.Components.Classes
+{
+	public class V2
+	{
+		public double X { get; set; }
+		public double Y { get; set; }
+
+		public V2()
+		{
+			X = 0;
+			Y = 0;
+		}
+
+		public V2(double x, double y)
+		{
+			X = x;
+			Y = y;
+		}
+	}
+}
---
+++ wwwroot/index.js
@@ -0,0 +1,9 @@
+﻿window.convertToSvgCoords = function (svgElement, screenX, screenY) {
+    let point = svgElement.createSVGPoint();
+    point.x = screenX;
+    point.y = screenY;
+
+    point = point.matrixTransform(svgElement.getScreenCTM().inverse());
+
+    return [point.x, point.y];
+};
